371adb2810462487a3db95485418decd31fa313f
jeronima <jeronima.floriano@totvs.com.br>
2022-10-31 19:36:13 -0300

    Criando o projeto, modelando a cidade e criando o repository

---
+++ src/main/java/localizacao/domain/entity/Cidade.java
@@ -0,0 +1,23 @@
+package localizacao.domain.entity;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_cidade")
+@Data
+public class Cidade {
+
+    @Id
+    @Column(name = "id_cidade")
+    private Long id;
+
+    @Column(length = 50, unique = true) //definindo tamanho 50 e que não pode ter outra cidade com msm nome
+    private String nome;
+
+    private Long habitantes;
+}
---
+++ src/main/java/localizacao/domain/repository/CidadeRepository.java
@@ -0,0 +1,7 @@
+package localizacao.domain.repository;
+
+import localizacao.domain.entity.Cidade;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CidadeRepository extends JpaRepository<Cidade, Long> {
+}
